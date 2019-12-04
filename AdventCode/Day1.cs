aa7b65cd26069f1815af91ecc511e5f7655ee4ce
James Higgins <jhiggins@miradorfin.com>
2019-12-04 08:19:39 -0800

    Day 2 part 1

---
+++ AdventCode/Day1.cs
@@ -0,0 +1,46 @@
+//using System;
+//using System.Collections.Generic;
+//using System.IO;
+//
+//namespace AdventCode
+//{
+//    public class Day1
+//    {
+//        static void Main(string[] args)
+//        {
+//            Console.WriteLine("Day 1");
+//            System.IO.StreamReader file = new System.IO.StreamReader("../../../../dayOneInput.txt");
+//
+//            int fuelRequirements = 0;
+//            string line;
+//
+//            while ((line = file.ReadLine()) != null)
+//            {
+//                int intLine = int.Parse(line);
+//                FuelCalculation(intLine);
+//            }
+//
+//            Console.WriteLine("Fuel Requirements = " + fuelRequirements);
+//
+//
+//            void FuelCalculation(int mass)
+//            {
+//                int num = mass;
+//                while (DivideByThreeAndSubtractTwo(num) > 0)
+//                {
+//                    int newNum = DivideByThreeAndSubtractTwo(num);
+//                    fuelRequirements += newNum;
+//                    num = newNum;
+//                }
+//            }
+//
+//            int DivideByThreeAndSubtractTwo(int inputNumber)
+//            {
+//                int massDivThree = inputNumber / 3;
+//                int numberAfterCalculation = massDivThree - 2;
+//
+//                return numberAfterCalculation;
+//            }
+//        }
+//    }
+//}
---
+++ AdventCode/Day2.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Security.Principal;
+
+namespace AdventCode
+{
+    class Day2
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Day 2");
+            StreamReader file = new StreamReader("../../../../dayTwoInput.txt");
+            string line = null;
+
+            while (null != (line = file.ReadLine()))
+            {
+                string[] values = line.Split(',');
+
+                List<int> intValues = values.Select(int.Parse).ToList();
+                for (int i = 0; i < intValues.Count; i += 4)
+                {
+                    if (intValues[i] == 99)
+                    {
+                        break;
+                    }
+
+                    if (intValues[i] == 1)
+                    {
+                        Console.WriteLine("OpCode: 1");
+
+                        int positionOneValue = intValues[i + 1];
+                        int lookedUpValuePositionOne = intValues[positionOneValue];
+                        int positionTwoValue = intValues[i + 2];
+                        int lookedUpValuePositionTwo = intValues[positionTwoValue];
+                        int positionThreeValue = intValues[i + 3];
+
+                        intValues[positionThreeValue] = lookedUpValuePositionOne + lookedUpValuePositionTwo;
+                    }
+
+                    if (intValues[i] == 2)
+                    {
+                        Console.WriteLine("OpCode: 2:");
+                        int positionOneValue = intValues[i + 1];
+                        int lookedUpValuePositionOne = intValues[positionOneValue];
+                        int positionTwoValue = intValues[i + 2];
+                        int lookedUpValuePositionTwo = intValues[positionTwoValue];
+                        int positionThreeValue = intValues[i + 3];
+
+                        intValues[positionThreeValue] = (lookedUpValuePositionOne * lookedUpValuePositionTwo);
+                    }
+                }
+
+                Console.WriteLine("Ending Position 0: " + intValues[0]);
+            }
+        }
+    }
+}
