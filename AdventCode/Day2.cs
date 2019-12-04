722212aeb144a22dee5bf0f25a32bce1034ca0db
James Higgins <jhiggins@miradorfin.com>
2019-12-04 08:53:14 -0800

    Refactor day 2 part 1

--- AdventCode/Day2.cs
+++ AdventCode/Day2.cs
@@ -8,51 +8,61 @@ namespace AdventCode
 {
     class Day2
     {
-        static void Main(string[] args)
-        {
-            Console.WriteLine("Day 2");
-            StreamReader file = new StreamReader("../../../../dayTwoInput.txt");
-            string line = null;
-
-            while (null != (line = file.ReadLine()))
+       
+         private static int Calculate(int[] intValues)
             {
-                string[] values = line.Split(',');
-
-                List<int> intValues = values.Select(int.Parse).ToList();
-                for (int i = 0; i < intValues.Count; i += 4)
+              
+                for (var i = 0; i < intValues.Length; i += 4)
                 {
                     if (intValues[i] == 99)
                     {
                         break;
                     }
-
+                    
                     if (intValues[i] == 1)
                     {
                         Console.WriteLine("OpCode: 1");
 
-                        int positionOneValue = intValues[i + 1];
-                        int lookedUpValuePositionOne = intValues[positionOneValue];
-                        int positionTwoValue = intValues[i + 2];
-                        int lookedUpValuePositionTwo = intValues[positionTwoValue];
-                        int positionThreeValue = intValues[i + 3];
+                        var positionOneValue = intValues[i + 1];
+                        var lookedUpValuePositionOne = intValues[positionOneValue];
+                        var positionTwoValue = intValues[i + 2];
+                        var lookedUpValuePositionTwo = intValues[positionTwoValue];
+                        var positionThreeValue = intValues[i + 3];
 
                         intValues[positionThreeValue] = lookedUpValuePositionOne + lookedUpValuePositionTwo;
                     }
 
-                    if (intValues[i] == 2)
+                    if (intValues[i] != 2) continue;
                     {
                         Console.WriteLine("OpCode: 2:");
-                        int positionOneValue = intValues[i + 1];
-                        int lookedUpValuePositionOne = intValues[positionOneValue];
-                        int positionTwoValue = intValues[i + 2];
-                        int lookedUpValuePositionTwo = intValues[positionTwoValue];
-                        int positionThreeValue = intValues[i + 3];
+                        var positionOneValue = intValues[i + 1];
+                        var lookedUpValuePositionOne = intValues[positionOneValue];
+                        var positionTwoValue = intValues[i + 2];
+                        var lookedUpValuePositionTwo = intValues[positionTwoValue];
+                        var positionThreeValue = intValues[i + 3];
 
                         intValues[positionThreeValue] = (lookedUpValuePositionOne * lookedUpValuePositionTwo);
                     }
                 }
 
                 Console.WriteLine("Ending Position 0: " + intValues[0]);
+                return intValues[0];
+            }
+
+         private static void Main(string[] args)
+        {
+            Console.WriteLine("Day 2");
+            var file = new StreamReader("../../../../dayTwoInput.txt");
+
+            string line = null;
+            while (null != (line = file.ReadLine()))
+            {
+                var values = line.Split(',');
+                var intValues = values.Select(int.Parse).ToList();
+
+                var answer = Calculate(intValues.ToArray());
+                    
+                Console.WriteLine(answer + " = answer");
             }
         }
     }
